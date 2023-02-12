e85380d1f4f6c6980b0b60b351ccabbdcc90b8b5
xMurzik <murzik6667@gmail.com>
2023-02-12 13:47:58 +0300

    Added header + button on him

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import sushi from './sushi.jpg';
+import style from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={style.header}>
+        <h1>Japan kitchen</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={style['main-image']}>
+        <img src={sushi} alt="sushi" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/Layout/HeaderCartButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './HeaderCartButton.module.css';
+import CartIcon from '../Cart/CartIcon';
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={style.button}>
+      <span className={style.icon}>
+        <CartIcon />
+      </span>
+      <span>Cart</span>
+      <span className={style.badge}>2</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;

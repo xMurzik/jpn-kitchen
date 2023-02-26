e91b914cbe1b67e06a5f09f371eb0b801fe8fdc6
xMurzik <murzik6667@gmail.com>
2023-02-26 15:50:17 +0300

    add database

--- src/components/Meals/MealList.jsx
+++ src/components/Meals/MealList.jsx
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './MealList.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Roll "Naomi"',
-    description:
-      'Philadelphia cheese, chicken fillet, masago, tomato, cucumber, sesame',
-    price: 11.99,
-  },
-  {
-    id: 'm2',
-    name: 'Spice in salmon',
-    description: 'Rice, salmon, spice sauce',
-    price: 3.99,
-  },
-  {
-    id: 'm3',
-    name: 'Sushi with eel',
-    description: 'Smoked eel, unagi sauce, sesame',
-    price: 4.99,
-  },
-  {
-    id: 'm4',
-    name: 'Salad "Poke with salmon"',
-    description: 'Rice, salmon, cucumber, chuka, nori, tuna chips, nut sauce',
-    price: 7.99,
-  },
-];
-
 const MealList = (props) => {
-  let mealList = DUMMY_MEALS.map((elem) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const [httpError, setHttpErrorMessage] = useState();
+
+  useEffect(() => {
+    let loadedMeals = [];
+    setIsloading(true);
+    fetch(
+      'https://react-practicing-776b5-default-rtdb.firebaseio.com/meals.json'
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Smth going wrong!');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        setMeals(loadedMeals);
+        setIsloading(false);
+      })
+      .catch((e) => {
+        setHttpErrorMessage(e.message);
+        setIsloading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={style.loading}>
+        <p>Loading data...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={style.error}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  let mealList = meals.map((elem) => (
     <MealItem
       id={elem.id}
       key={elem.id}
